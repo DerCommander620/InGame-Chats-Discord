39ebbab70ef318a2af55a494c3289c3a1c80a940
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-07-09 18:34:56 +0200

    Update Main.php

--- src/DerCommander610/Incord/Main.php
+++ src/DerCommander610/Incord/Main.php
@@ -3,12 +3,13 @@ namespace DerCommander610\Incord;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
+use DerCommander610\Incord\EventListener;
 
 class Main extends PluginBase{
     use SingletonTrait;
 
     public function onEnable(): void{
         self::setInstance($this);
-        $this->getServer()->getPluginManager()->registerEvents();
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener);
     }
 }
